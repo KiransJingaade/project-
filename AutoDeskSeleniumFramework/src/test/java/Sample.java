99bac5d7787bafd940b00e2852a53d1946a55058
Kiran <kiran.s7903@gmail.com>
2023-06-10 16:03:43 +0530

    new

--- AutoDeskSeleniumFramework/src/test/java/Sample.java
+++ AutoDeskSeleniumFramework/src/test/java/Sample.java
@@ -7,7 +7,8 @@ public class Sample {
 	     String dateAndTime = date.toString();
 	             
 	     String YYYY = dateAndTime.split(" ")[5];
-	     String DD = dateAndTime.split(" ")[2];
+	     String DD= dateAndTime.split(" ")[2];
+	     System.out.println("Enter text for month");
 	           System.out.println(date.getMonth());
 	     
 	}
